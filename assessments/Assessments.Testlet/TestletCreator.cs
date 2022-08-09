daf8da70a33a2a4683b5f15789a0c565af6e6c64
Ravi515423973 <100338391+Ravi515423973@users.noreply.github.com>
2022-08-09 20:39:40 +0530

    Add files via upload

---
+++ assessments/Assessments.Testlet/TestletCreator.cs
@@ -0,0 +1,29 @@
+﻿namespace Assessments.Testlet
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class TestletCreator : ITestletCreator
+    {
+        private readonly ITestletValidator validator;
+        private readonly ITestletItemsRandomizer randomizer;
+
+        public TestletCreator(ITestletValidator validator, ITestletItemsRandomizer randomizer)
+        {
+            this.validator = validator ?? throw new ArgumentNullException(nameof(validator));
+            this.randomizer = randomizer ?? throw new ArgumentNullException(nameof(randomizer));
+        }
+
+        public Testlet CreateTestlet(string testletId, IReadOnlyList<Item> items)
+        {
+            _ = testletId ?? throw new ArgumentNullException(nameof(testletId));
+            _ = items ?? throw new ArgumentNullException(nameof(items));
+
+            this.validator.ValidateTestletCreationInput(testletId, items);
+
+            var randomItems = this.randomizer.Randomize(items);
+
+            return new Testlet(testletId, randomItems);
+        }
+    }
+}
